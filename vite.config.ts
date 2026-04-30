dcb4c84655ed1e46666cdc0766819302c5383f1b
Plant-Paramour <2024212110@stu.cqupt.edu.cn>
2026-04-30 18:14:16 +0800

    main

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/pale-notes/', // For GitHub Pages
+  base: '/pale-notes-LLMfree/', // For GitHub Pages
 })
